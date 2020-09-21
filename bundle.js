1e52bbc7215fe8eea072925db7a4c38a6149bc65
httpLucasReis <manoel.reisiema@gmail.com>
2020-09-21 00:20:23 -0300

    Adding local multiplayer

--- bundle.js
+++ bundle.js
@@ -13,9 +13,7 @@ var Snake = function () {
     this.pX = _pX;
     this.pY = _pY;
 
-    // Size of snake and color
-    //this.width = _width;
-    //this.height = _height;
+    // Snake  color
     this.color = _color;
 
     // Velocity
@@ -34,9 +32,13 @@ var Snake = function () {
     this.keyCodeUp = _keyCodeUp;
     this.keyCodeRight = _keyCodeRight;
     this.keyCodeDown = _keyCodeDown;
+
+    // Game rules
+    this.hit = false;
   }
 
   // methods
+
 
   _createClass(Snake, [{
     key: "move",
@@ -45,10 +47,9 @@ var Snake = function () {
         game.context.fillRect(this.trail[i].x * this.size, this.trail[i].y * this.size, this.size, this.size);
         // collied
         if (this.trail[i].x == this.pX && this.trail[i].y == this.pY) {
-          console.log("$ Rabo X: " + this.trail[i].x + " Rabo y: " + this.trail[i].y);
           this.velX = this.velY = 0;
           this.tail = 5;
-          return true;
+          this.hit = true;
         }
       }
 
@@ -60,16 +61,12 @@ var Snake = function () {
       while (this.trail.length > this.tail) {
         this.trail.shift();
       }
-
-      // don't collied
-      return false;
     }
   }, {
     key: "collisionFruit",
     value: function collisionFruit(fruit, distance) {
       if (this.pX == fruit.pX && this.pY == fruit.pY) {
         this.tail++;
-
         fruit.pX = Math.floor(Math.random() * distance);
         fruit.pY = Math.floor(Math.random() * distance);
       }
@@ -170,9 +167,28 @@ var Stage = function () {
 
   _createClass(Stage, [{
     key: "render",
-    value: function render(object) {
-      this.context.fillStyle = object.color;
-      this.context.fillRect(object.pX * object.size, object.pY * object.size, object.size, object.size);
+    value: function render(objects) {
+      var _this = this;
+
+      objects.forEach(function (object) {
+        _this.context.fillStyle = object.color;
+        _this.context.fillRect(object.pX * object.size, object.pY * object.size, object.size, object.size);
+      });
+    }
+  }, {
+    key: "restart",
+    value: function restart(snakes) {
+      var _this2 = this;
+
+      snakes.forEach(function (snake) {
+        snake.hit = false;
+        snake.velX = 1;
+        snake.velY = 0;
+        snake.pX = Math.floor(Math.random() * _this2.amountP);
+        snake.pY = Math.floor(Math.random() * _this2.amountP);
+        snake.tail = 0;
+        snake.trail = [];
+      });
     }
   }, {
     key: "message",
@@ -201,27 +217,77 @@ var canvas = document.querySelector("#stage");
 
 var game = new Stage(canvas, "2d", 40);
 
-var background = new Background("black", 0, 0, 400);
+var background = new Background("#BFC9CA", 0, 0, 400);
 
-var s1 = new Snake(0, 0, "#1D8348", 1, 0, 0, 0, [], 10, 37, 38, 39, 40);
+var s1 = new Snake(15, 20, "#3498DB", 1, 0, 0, 0, [], 10, 37, 38, 39, 40);
 
-var s2 = new Snake(20, 20, "#1D8334", 1, 0, 0, 0, [], 10, 65, 87, 68, 83);
+var s2 = new Snake(25, 20, "#E74C3C", 1, 0, 0, 0, [], 10, 65, 87, 68, 83);
 
-var f1 = new Fruit("yellow", 5, 5, 10);
+var f1 = new Fruit("#FDFEFE", 5, 5, 10);
 
 // Events of keyboard
 document.addEventListener("keydown", function (event) {
   game.key = event.keyCode;
 });
 
-var loop = setInterval(function () {
+// Function for to run game.
+function run() {
+  game.render([background, s1, s2, f1]);
+
+  if (!s1.hit && !s2.hit) {
+    s1.move(game);
+    s2.move(game);
+
+    s1.modifyVel(game.key);
+    s2.modifyVel(game.key);
+
+    s1.modifyPosition();
+    s2.modifyPosition();
+
+    s1.collisionScreen(game.amountP);
+    s2.collisionScreen(game.amountP);
+
+    s1.collisionFruit(f1, game.amountP);
+    s2.collisionFruit(f1, game.amountP);
+
+    game.message("#3498DB", "700 14px Arial", "First player: " + s1.trail.length, 40, 380);
+    game.message("#E74C3C", "700 14px Arial", "Second player: " + s2.trail.length, 240, 380);
+  } else if (s1.hit) {
+    console.log("s1.hit = " + s1.hit);
+    console.log("Estou no segundo if");
+    game.render([background]);
+    game.message("#E74C3C", "700 20px Arial", "Second player wins!", 105, 210);
+    clearInterval(loop);
+  } else {
+    game.render([background]);
+    game.message("#3498DB", "700 20px Arial", "First player wins!", 120, 210);
+    clearInterval(loop);
+  }
+}
+
+// Game loop
+var loop = setInterval(run, 50);
+
+// Restart game 
+var restartButton = document.querySelector("#restart");
+restartButton.addEventListener("click", function () {
+  clearInterval(loop);
+  game.restart([s1, s2]);
+  loop = setInterval(run, 50);
+});
+
+/* const loop = function() {
+
+  console.log(`Posição x: ${s1.pX} Posição Y: ${s1.pY}`);
   game.render(background);
   game.render(s1);
   game.render(s2);
   game.render(f1);
 
-  var hit = s1.move(game);
-  var hit2 = s2.move(game);
+  const hit = s1.move(game);
+  const hit2 = s2.move(game);
+
+  requestAnimationFrame(loop);
 
   if (!hit && !hit2) {
     s1.modifyVel(game.key);
@@ -235,30 +301,18 @@ var loop = setInterval(function () {
 
     s1.collisionFruit(f1, game.amountP);
     s2.collisionFruit(f1, game.amountP);
+
+    game.message("#fff", "700 14px Arial", `First player: ${s1.trail.length}`, 40, 380);
+    game.message("#fff", "700 14px Arial", `Second player: ${s2.trail.length}`, 240, 380);
+
+  } else if(hit){
+    game.render(background);
+    game.message("yellow", "700 20px Arial", "Second player wins!", 110, 210);
   } else {
     game.render(background);
-    game.message("yellow", "700 20px Arial", "GAME OVER", 140, 210);
+    game.message("yellow", "700 20px Arial", "First player wins!", 120, 210);
   }
-}, 60);
-
-/* const loop = ()=>{
-    console.log(`pX: ${s1.pX} pY: ${s1.pY}`);
-    game.render(background);
-    game.render(s1);
-    game.render(f1);
-
-    const hit = s1.move(game);
-
-    if (!hit) {
-        s1.collisionFruit(f1, 20);
-        s1.collisionScreen(20);
-        s1.modifyVel(game.keyPressed);
-        s1.modifyPosition();
-        game.message("pink", "700 20px Arial", "TEXTO TEXTO TEXTO", 140, 210);
-    } else {
-        game.render(background);
-        game.message("yellow", "700 20px Arial", "GAME OVER", 140, 210);
-    }
 }
 
-requestAnimationFrame(loop); */
+requestAnimationFrame(loop);
+ */
--- game.js
+++ game.js
@@ -5,9 +5,7 @@ class Snake {
     this.pX = _pX;
     this.pY = _pY;
 
-    // Size of snake and color
-    //this.width = _width;
-    //this.height = _height;
+    // Snake  color
     this.color = _color;
 
     // Velocity
@@ -27,10 +25,12 @@ class Snake {
     this.keyCodeRight = _keyCodeRight;
     this.keyCodeDown = _keyCodeDown;
 
+    // Game rules
+    this.hit = false;
+
   }
 
   // methods
-
   move(game) {
     for (let i = 0; i < this.trail.length; i++) {
       game.context.fillRect(
@@ -41,10 +41,9 @@ class Snake {
       );
       // collied
       if (this.trail[i].x == this.pX && this.trail[i].y == this.pY) {
-        console.log(`$ Rabo X: ${this.trail[i].x} Rabo y: ${this.trail[i].y}`);
         this.velX = this.velY = 0;
         this.tail = 5;
-        return true;
+        this.hit = true;
       }
     }
 
@@ -56,15 +55,11 @@ class Snake {
     while (this.trail.length > this.tail) {
       this.trail.shift();
     }
-
-    // don't collied
-    return false;
   }
 
   collisionFruit(fruit, distance) {
     if (this.pX == fruit.pX && this.pY == fruit.pY) {
       this.tail++;
-
       fruit.pX = Math.floor(Math.random() * distance);
       fruit.pY = Math.floor(Math.random() * distance);
     }
@@ -147,15 +142,29 @@ class Stage {
   }
 
   // Methods
-  render(object) {
-    this.context.fillStyle = object.color;
-    this.context.fillRect(
-      object.pX * object.size,
-      object.pY * object.size,
-      object.size,
-      object.size
-    );
+  render(objects) {
+    objects.forEach((object)=>{
+      this.context.fillStyle = object.color;
+      this.context.fillRect(
+        object.pX * object.size,
+        object.pY * object.size,
+        object.size,
+        object.size
+      );
+    })
   }
+
+  restart(snakes){
+    snakes.forEach((snake)=>{
+      snake.hit = false;
+      snake.velX = 1;
+      snake.velY = 0;
+      snake.pX = Math.floor(Math.random() * this.amountP);
+      snake.pY = Math.floor(Math.random() * this.amountP);
+      snake.tail = 0;
+      snake.trail = [];
+    })
+  } 
 
   set key(key) {
     this.keyPressed = key;
@@ -177,20 +186,72 @@ const canvas = document.querySelector("#stage");
 
 const game = new Stage(canvas, "2d", 40);
 
-const background = new Background("black", 0, 0, 400);
+const background = new Background("#BFC9CA", 0, 0, 400);
 
-const s1 = new Snake(0, 0, "#1D8348", 1, 0, 0, 0, [], 10, 37, 38, 39, 40);
+const s1 = new Snake(15, 20, "#3498DB", 1, 0, 0, 0, [], 10, 37, 38, 39, 40);
 
-const s2 = new Snake(20,20, "#1D8334", 1, 0, 0, 0, [], 10, 65, 87, 68, 83);
+const s2 = new Snake(25, 20, "#E74C3C", 1, 0, 0, 0, [], 10, 65, 87, 68, 83);
 
-const f1 = new Fruit("yellow", 5, 5, 10);
+const f1 = new Fruit("#FDFEFE", 5, 5, 10);
+
 
 // Events of keyboard
 document.addEventListener("keydown", (event) => {
   game.key = event.keyCode;
 });
 
-const loop = setInterval(() => {
+
+// Function for to run game.
+function run(){
+  game.render([background, s1, s2, f1])
+
+  if (!s1.hit && !s2.hit) {
+    s1.move(game);
+    s2.move(game);
+
+    s1.modifyVel(game.key);
+    s2.modifyVel(game.key);
+
+    s1.modifyPosition();
+    s2.modifyPosition();
+
+    s1.collisionScreen(game.amountP);
+    s2.collisionScreen(game.amountP);
+
+    s1.collisionFruit(f1, game.amountP);
+    s2.collisionFruit(f1, game.amountP);
+
+    game.message("#3498DB", "700 14px Arial", `First player: ${s1.trail.length}`, 40, 380);
+    game.message("#E74C3C", "700 14px Arial", `Second player: ${s2.trail.length}`, 240, 380);
+
+  } else if(s1.hit){
+    console.log(`s1.hit = ${s1.hit}`)
+    console.log("Estou no segundo if");
+    game.render([background]);
+    game.message("#E74C3C", "700 20px Arial", "Second player wins!", 105, 210);
+    clearInterval(loop);
+  } else {
+    game.render([background]);
+    game.message("#3498DB", "700 20px Arial", "First player wins!", 120, 210);
+    clearInterval(loop);
+  }
+}
+
+// Game loop
+let loop = setInterval(run, 50); 
+
+
+// Restart game 
+const restartButton = document.querySelector("#restart");
+restartButton.addEventListener("click", ()=>{
+  clearInterval(loop);
+  game.restart([s1,s2]);
+  loop = setInterval(run, 50);
+})
+
+/* const loop = function() {
+
+  console.log(`Posição x: ${s1.pX} Posição Y: ${s1.pY}`);
   game.render(background);
   game.render(s1);
   game.render(s2);
@@ -198,6 +259,8 @@ const loop = setInterval(() => {
 
   const hit = s1.move(game);
   const hit2 = s2.move(game);
+
+  requestAnimationFrame(loop);
 
   if (!hit && !hit2) {
     s1.modifyVel(game.key);
@@ -212,30 +275,17 @@ const loop = setInterval(() => {
     s1.collisionFruit(f1, game.amountP);
     s2.collisionFruit(f1, game.amountP);
 
+    game.message("#fff", "700 14px Arial", `First player: ${s1.trail.length}`, 40, 380);
+    game.message("#fff", "700 14px Arial", `Second player: ${s2.trail.length}`, 240, 380);
+
+  } else if(hit){
+    game.render(background);
+    game.message("yellow", "700 20px Arial", "Second player wins!", 110, 210);
   } else {
     game.render(background);
-    game.message("yellow", "700 20px Arial", "GAME OVER", 140, 210);
+    game.message("yellow", "700 20px Arial", "First player wins!", 120, 210);
   }
-}, 60);
-
-/* const loop = ()=>{
-    console.log(`pX: ${s1.pX} pY: ${s1.pY}`);
-    game.render(background);
-    game.render(s1);
-    game.render(f1);
-
-    const hit = s1.move(game);
-
-    if (!hit) {
-        s1.collisionFruit(f1, 20);
-        s1.collisionScreen(20);
-        s1.modifyVel(game.keyPressed);
-        s1.modifyPosition();
-        game.message("pink", "700 20px Arial", "TEXTO TEXTO TEXTO", 140, 210);
-    } else {
-        game.render(background);
-        game.message("yellow", "700 20px Arial", "GAME OVER", 140, 210);
-    }
 }
 
-requestAnimationFrame(loop); */
+requestAnimationFrame(loop);
+ */
